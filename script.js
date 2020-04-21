f5864cfe4bbb63b5126d0362595f83f67b5fc318
plutownium <rolandmackintosh@gmail.com>
2020-04-20 18:30:29 -0700

    wrote a func, resetEventListeners, to rm all event listeners & replace w/ replaceWithWall listeners

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+// <^> <^><^> <^><^> <^><^> <^><^> <^><^> <^><^> <^><^> <^><^> <^><^> <^>
+// <^> <^><^> <^><^> <^><^> <^><^> <^><^> <^><^> <^><^> <^><^> <^><^> <^>
+// <^> <^><^> <^><^> <^><^> <^><^> <^><^> <^><^> <^><^> <^><^> <^><^> <^>
 // ### startup... render the page
 const mainDiv = document.getElementById("main");
 
+const START_NODE = "*";
+const EMPTY_SPACE = ".";
 const WALL_SEGMENT = "#";
 
 // generate a n by m grid of .'s
@@ -33,6 +38,29 @@ for (let j = 0; j < numOfColumns; j++) {
 	}
 }
 
+// ### populate the grid of divs with ascii
+
+for (let i = 0; i < numOfRows; i++) {
+	for (let j = 0; j < numOfColumns; j++) {
+		const targetDiv = getLocationByCoordinates(j, i);
+
+		targetDiv.innerHTML = grid[i][j];
+	}
+}
+
+// ### Add event listeners so user can click to add a wall segment.
+resetEventListeners();
+
+// ### Let user pick an Origin point for the algorithm
+const moveOriginBtn = document.getElementById("moveStartBtn");
+moveOriginBtn.addEventListener("click", () => {
+	nextClickMovesOrigin();
+});
+
+// ### Let user add a Target
+
+// ### Let user switch between moving the target, adding a wall, moving the Origin point.
+
 function getLocationByCoordinates(x, y) {
 	// NOTES: Remember the columns and rows are Zero Indexed! Cols 0 through 9 and Rows 0 through 7 in the draft version
 	const getMainDiv = document.getElementById("main");
@@ -41,45 +69,11 @@ function getLocationByCoordinates(x, y) {
 	return target;
 }
 
-console.log(getLocationByCoordinates(10, 0));
-
-// ### populate the grid of divs with ascii
-console.log(numOfColumns, numOfRows);
-
-for (let i = 0; i < numOfRows; i++) {
-	for (let j = 0; j < numOfColumns; j++) {
-		const targetDiv = getLocationByCoordinates(j, i);
-
-		// console.log(i, j);
-		// if (grid[i] === undefined) {
-		// 	console.log("Num of columns:" + numOfColumns);
-		// 	console.log("Num of rows:" + numOfRows);
-		// 	console.log("300:" + grid[i]);
-		// }
-		// console.log(grid[i][j]);
-		targetDiv.innerHTML = grid[i][j];
-	}
-}
-
-// ### Add event listeners so user can click to add a wall segment.
-// Pass div position as an arg to the func so it knows where to do the replacement.
-// Replace the Grid's text, then rerender the display. DO NOT just change the text in the DOM w/o re-rendering based on the Grid.
-const populatedColumns = mainDiv.children;
-for (let x = 0; x < numOfColumns; x++) {
-	const targetCol = populatedColumns[x];
-	for (let y = 0; y < numOfRows; y++) {
-		targetCol.children[y].addEventListener("click", () => {
-			// would like to use the replaceWithWall() func here, but can't figure out how to do it.
-			grid[y][x] = WALL_SEGMENT;
-			rerenderGrid();
-		});
-	}
-}
-
 function replaceWithWall(x, y) {
 	// NOTE: Grid coordinates are y, x not x, y like you'd expect
-
-	grid[y][x] = WALL_SEGMENT;
+	if (grid[y][x] === EMPTY_SPACE) {
+		grid[y][x] = WALL_SEGMENT;
+	}
 	rerenderGrid();
 }
 
@@ -87,8 +81,75 @@ function rerenderGrid() {
 	for (let i = 0; i < numOfRows; i++) {
 		for (let j = 0; j < numOfColumns; j++) {
 			const targetDiv = getLocationByCoordinates(j, i);
-
 			targetDiv.innerHTML = grid[i][j];
 		}
 	}
 }
+
+function nextClickMovesOrigin() {
+	// function allows the user to move the Start Node around on the grid.
+	// First all event listeners are removed so there isn't conflict between "add wall" and "add start node".
+	// Then each spot on the grid gets an addStartNode event listener.
+	// The "addStartNode" function will take care of replacing all spots on the grid with "add wall" event listeners when finished.
+
+	const columnDivs = mainDiv.children;
+	for (let x = 0; x < numOfColumns; x++) {
+		// iterate through the columns, getting a list of their children
+		const targetColumnRows = columnDivs[x].children;
+		for (let y = 0; y < numOfRows; y++) {
+			// iterate through the row divs in the columns.
+			// To remove all event listeners, clone the node, and replace it with the clone.
+			const oldElement = targetColumnRows[y];
+			const newElement = oldElement.cloneNode(true);
+			oldElement.parentNode.replaceChild(newElement, oldElement);
+		}
+		for (let y = 0; y < numOfRows; y++) {
+			targetColumnRows[y].addEventListener("click", () => {
+				addStartNode(x, y);
+			});
+		}
+	}
+}
+
+function addStartNode(x, y) {
+	// NOTE: Grid coordinates are y, x not x, y like you'd expect
+	grid[y][x] = START_NODE;
+	rerenderGrid();
+	resetEventListeners();
+}
+
+function resetEventListeners() {
+	// general purpose function used to
+	// 1) remove all event listeners from grid and
+	// 2) add "Replace With Wall" event listeners to each grid pos, which is the default state.
+
+	const columnDivs = mainDiv.children;
+	for (let x = 0; x < numOfColumns; x++) {
+		// iterate through the columns, getting a list of their children
+		const targetColumnRows = columnDivs[x].children;
+		for (let y = 0; y < numOfRows; y++) {
+			// iterate through the row divs in the columns.
+			// To remove all event listeners, clone the node, and replace it with the clone.
+			const oldElement = targetColumnRows[y];
+			const newElement = oldElement.cloneNode(true);
+			oldElement.parentNode.replaceChild(newElement, oldElement);
+		}
+		for (let y = 0; y < numOfRows; y++) {
+			targetColumnRows[y].addEventListener("click", () => {
+				replaceWithWall(x, y);
+			});
+		}
+	}
+}
+
+// a button for printing the grid's current first 4 values
+const testButton = document.getElementById("printGrid");
+testButton.addEventListener("click", () => {
+	for (i = 0; i < 4; i++) {
+		console.log(grid[i]);
+	}
+});
+
+// <^> <^><^> <^><^> <^><^> <^><^> <^><^> <^><^> <^><^> <^><^> <^><^> <^>
+// <^> <^><^> <^><^> <^><^> <^><^> <^><^> <^><^> <^><^> <^><^> <^><^> <^>
+// <^> <^><^> <^><^> <^><^> <^><^> <^><^> <^><^> <^><^> <^><^> <^><^> <^>
